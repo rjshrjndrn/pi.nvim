543556942aca086eecc1217fc8a5b5cb263b165d
rjshrjndrn <rjshrjndrn@gmail.com>
2026-03-29 18:37:46 +0200

    chore(config): Adding config
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

---
+++ lua/pi/config.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+M.defaults = {
+	split = {
+		position = "right",
+		width = 0.35,
+	},
+	keymaps = {
+		ask = "<leader>ap",
+		toggle = "<leader>pp",
+	},
+	pi = {
+		bin = "pi",
+		extra_args = {},
+	},
+}
+
+M.options = {}
+
+function M.setup(opts)
+	M.options = vim.tbl_deep_extend("force", M.defaults, opts or {})
+end
+
+return M
